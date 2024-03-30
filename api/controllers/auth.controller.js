2af870348ec94714b266318989910436f4c06cf4
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-03-30 12:28:13 +0100

    create sign up page ui

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,25 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
-    if (!username || !email || !password || username === "" || email === "" || password === "") {
-        return res.status(400).json({ message: "All fields are required" });
+    // Comprueba si username, email o password están vacíos. Si es así, llama a la función errorHandler con un código de estado 400 y un mensaje de error, y termina la ejecución de la función.
+    if (!username || 
+        !email || 
+        !password || 
+        username === "" || 
+        email === "" || 
+        password === ""
+        ) {
+        next(errorHandler(400, "All fields are required"));
     }
 
+    // Encripta la contraseña utilizando bcryptjs. El '10' es el número de rondas que bcryptjs utilizará para generar el "salt". Un salt es una cadena aleatoria que se añade a la contraseña antes de hashearla para evitar ataques de diccionario y de tabla arco iris.
     const hashedPassword = bcryptjs.hashSync(password, 10);
 
+    // Crea una nueva instancia de User con username, email y la contraseña encriptada.
     const newUser = new User({ 
         username, 
         email, 
@@ -17,9 +27,11 @@ export const signup = async (req, res) => {
     });
 
     try {
+        // Intenta guardar el nuevo usuario en la base de datos. Si la operación es exitosa, envía una respuesta con el mensaje "User created".
         await newUser.save();
         res.json("User created");
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        // Si ocurre un error al guardar el usuario, pasa el error al siguiente middleware de manejo de errores.
+        next(error);
     }
 };
