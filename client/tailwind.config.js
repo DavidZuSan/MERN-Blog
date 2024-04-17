8861b65493a86da8d048574feff9b722872aeb34
David Zurita <108833714+DavidZuSan@users.noreply.github.com>
2024-04-17 19:23:02 +0200

    show user posts inside dashboard

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import flowbitePlugin from "flowbite/plugin";
+import tailwindScrollbar from "tailwind-scrollbar";
 
 export default {
   content: [
@@ -11,7 +12,8 @@ export default {
     extend: {},
   },
   plugins: [
-    flowbitePlugin
-  ],
+    flowbitePlugin,
+    tailwindScrollbar,
+  ]
 }
 
